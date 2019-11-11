19d10d44ae6bc1b6da5799230ca9a4a43bc313ec
Aakarsh Arya <aakiarya@yahoo.com>
2019-11-11 18:45:44 -0500

    changed database file

--- SecureBox/chalicelib/db.py
+++ SecureBox/chalicelib/db.py
@@ -1,29 +1,75 @@
-import os
-from pynamodb.models import Model
-from pynamodb.attributes import (
-    UnicodeAttribute, UnicodeSetAttribute, BooleanAttribute
-)
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+import botocore
 
-CURRENT_ENV = os.environ.get('CURRENT_ENV', 'dev')
+dynamodb = boto3.resource('dynamodb')
 
-class Database(Model):
-    class Meta:
-        table_name = 'SecureBoxClients'
-        read_capacity_units = 2
-        write_capacity_units = 2
-        if CURRENT_ENV == 'dev':
-            host = 'http://127.0.0.1:8001'
-        else:
-            region = 'us-east-2'
-
-    m_BoxID = UnicodeAttribute(hash_key=True)
-    m_PhoneNumber = UnicodeAttribute()
-    m_Email = UnicodeAttribute()
-    m_Password = UnicodeAttribute()
-    m_Orders = UnicodeSetAttribute(null=True)
-    m_Locked = BooleanAttribute(null=True)
+class Database:
+    m_table = dynamodb.Table('ClientData')
+    m_size = 0
+    #def authenticate(self, boxID):
 
 
+    def register(self, boxID, phone_number, email, password, orders, access_code, lock_status):
+        item = {
+            'box_id': boxID,
+            'access_code': access_code,
+            'email': email,
+            'orders': orders,
+            'password': access_code,
+            'phone_number': phone_number,
+            'locked': lock_status
+        }
+        try:
+            self.m_table.put_item(
+            Item=item,
+            ConditionExpression='attribute_not_exists(box_id)'
+            )
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+                print('This box_id already exists.')
 
-        
+    def deleteItem(self, boxID):
+        self.m_table.delete_item(
+            Key = {'box_id':boxID}
+        )
+    
+    def addOrder(self, boxID, tracking_id):
+        self.m_table.update_item(
+            Key = {'box_id':boxID},
+            UpdateExpression = 'ADD orders :new_order',
+            ExpressionAttributeValues = {':new_order': {tracking_id}}
+        )
+    
+    def deleteOrder(self, boxID, tracking_id):
+        self.m_table.update_item(
+            Key = {'box_id':boxID},
+            UpdateExpression = 'DELETE orders :order',
+            ExpressionAttributeValues = {':order': {tracking_id}}
+        )
 
+    def setLockStatus(self, boxID, lockStatus):
+        self.m_table.update_item(
+            Key = {'box_id': boxID},
+            UpdateExpression = 'SET locked = :status',
+            ExpressionAttributeValues = {':status': lockStatus}
+        )
+
+    def setAccessCode(self, boxID, access_code):
+        self.m_table.update_item(
+            Key = {'box_id': boxID},
+            UpdateExpression = 'SET access_code = :code',
+            ExpressionAttributeValues = {':code': access_code}
+        )
+
+    def getItem(self, boxID):
+        item = self.m_table.get_item(
+            Key={'box_id':boxID}
+        )
+        return item['Item']
+
+    def getLockStatus(self, boxID):
+        item = self.getItem(boxID)
+        return item['locked']
+
+db = Database()
